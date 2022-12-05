0d5ea89122122f7b89d38bd68477a35ee553e65a
Mary Bunde <marybunde@Marys-MacBook-Air.local>
2022-12-04 19:23:09 -0500

    working on controllers

--- Client/src/main/java/controllers/MessageController.java
+++ Client/src/main/java/controllers/MessageController.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import models.Id;
 import models.Message;
 
@@ -24,8 +27,16 @@ public class MessageController {
         return null;
     }
 
-    public Message postMessage(Id myId, Id toId, Message msg) {
-        return null;
+    public Message postMessage(Id myId, Id toId, String msg) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String to = (told != null) ? told.getGitHubId() :"";
+        String payload = mapper.writeValueAsString(new Message(myId.getGitHubId(),to,msg));
+
+        String result = transactionController.post(String.format("/ids/%s/messages",myId.getGithubId()),payload);
+        System.out.println(result);
+
+
+        return mapper.readValue(result,Message.class);
     }
  
 }
